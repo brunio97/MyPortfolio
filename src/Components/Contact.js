ce08de1b3abce12867e6d7eff383b982e6f72bad
brunio97 <brusalinasv1997@gmail.com>
2024-04-11 15:46:49 -0400

    testEt

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -50,7 +50,27 @@ return (
         </Col>
         <Col className="der" size={12} md={6}>
             <h2>Get In Touch</h2>
-              <form name="Contacthelp" onSubmit="submit" method="POST" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+              {/* <form name="contact" netlify>
                 <Row>
                   <Col size={12} sm={6} className="px-1">
                     <input type="text"  placeholder="First Name" name="first-name" id="first-name"/>
@@ -75,7 +95,7 @@ return (
                     </Col>
                   }
                 </Row>
-              </form>
+              </form> */}
         </Col>
       </Row>
     </Container>
