2852a82fd4f02c866c8c5f7f93b25119138e27b1
brunio97 <brusalinasv1997@gmail.com>
2024-04-12 17:45:39 -0400

    test

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -39,7 +39,6 @@ return(
             <Nav.Link href="#skills" className={activeLink === 'skills'? 'active navbar-link': 'navbar-links'} onClick={()=> onUpdateLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects'? 'active navbar-link': 'navbar-links'} onClick={()=> onUpdateLink('projects')}>Projects</Nav.Link>
           </Nav>
-
           <span className="navbar-text">
             <div className="social-icon">
               <a href="https://github.com/brunio97"><FaGithub className="fa-icon"/></a>
--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -5,7 +5,7 @@ function ProjectCard ({title,description,imgUrl,link}){
     return (
    
             <div className="proj-imgbx">
-                <a href={link}><img src={imgUrl} />
+                <a href={link}target="_blank"  ><img src={imgUrl} />
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
