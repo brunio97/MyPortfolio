c925e72b22b7e87130ebf8070e74b03fde4f08b9
brunio97 <brusalinasv1997@gmail.com>
2024-04-12 08:28:32 -0400

    js/netlify

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,43 +3,58 @@ import { Container, Row, Col } from "react-bootstrap";
 import SendMail from '../assets/SendMail.gif'
 
 function Contact (){
-    const formInitialDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: ''
-      }
-    const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const [buttonText, setButtonText] = useState('Send');
-    const [status, setStatus] = useState({});
     
-    const onFormUpdate = (category, value) => {
-          setFormDetails({
-            ...formDetails,
-            [category]: value
-          })
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+  
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => navigate("/thank-you/"))
+      .catch((error) => alert(error));
+  };
+  // const formInitialDetails = {
+    //     firstName: '',
+    //     lastName: '',
+    //     email: '',
+    //     phone: '',
+    //     message: ''
+    //   }
+    // const [formDetails, setFormDetails] = useState(formInitialDetails);
+    // const [buttonText, setButtonText] = useState('Send');
+    // const [status, setStatus] = useState({});
     
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json;charset=utf-8",
-          },
-          body: JSON.stringify(formDetails),
-        });
-        setButtonText("Send");
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code == 200) {
-          setStatus({ succes: true, message: 'Message sent successfully'});
-        } else {
-          setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-        }
-    };
+    // const onFormUpdate = (category, value) => {
+    //       setFormDetails({
+    //         ...formDetails,
+    //         [category]: value
+    //       })
+    // }
+    
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     setButtonText("Sending...");
+    //     let response = await fetch("http://localhost:5000/contact", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json;charset=utf-8",
+    //       },
+    //       body: JSON.stringify(formDetails),
+    //     });
+    //     setButtonText("Send");
+    //     let result = await response.json();
+    //     setFormDetails(formInitialDetails);
+    //     if (result.code == 200) {
+    //       setStatus({ succes: true, message: 'Message sent successfully'});
+    //     } else {
+    //       setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+    //     }
+    // };
     
 return (
     <section className="contact" id="connect">
@@ -51,25 +66,25 @@ return (
         <Col className="der" size={12} md={6}>
             <h2>Get In Touch</h2>
             <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Your Role: <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
               {/* <form name="contact" netlify>
                 <Row>
                   <Col size={12} sm={6} className="px-1">
